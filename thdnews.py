1ea3ccacb7843df2095a655488c6cf47826a4279
Furai <furai@thd.vg>
2015-06-30 14:50:17 +0200

    [fixed] errors with missing settings scope [testing] printing tweets from users

--- thdnews.py
+++ thdnews.py
@@ -7,10 +7,10 @@ if not hasattr(settings, 'ACCESS_TOKEN'):
 	twitter = Twython(settings.APP_KEY, settings.APP_SECRET, oauth_version=2)
 	accToken = twitter.obtain_access_token()
 	print "Edit your settings.py and add the line below to it:"
-	print "ACCES_TOKEN = \"%s\"" % accToken
+	print "ACCESS_TOKEN = \"%s\"" % accToken
 else:
-	print "Everything seems to be fine, boss."
-	twitter = Twython(APP_KEY, access_token=settings.ACCESS_TOKEN)
-	search = twitter.search_gen('python')
-	for result in search:
-    	print result
+	twitter = Twython(settings.APP_KEY, access_token=settings.ACCESS_TOKEN)
+	for user in settings.USER:
+		user_timeline=twitter.get_user_timeline(screen_name=user, count=10, exclude_replies=True, include_rts=False)
+		for tweet in user_timeline:
+			print tweet + "\n"
