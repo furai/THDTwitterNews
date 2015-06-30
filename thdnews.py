ae6fee597416cab2fcabb9c593a649fe76f4857b
Furai <furai@thd.vg>
2015-06-30 12:15:30 +0200

    More test.

--- thdnews.py
+++ thdnews.py
@@ -1,7 +1,7 @@
 import tweepy
 import settings
 
-if not hasattr(settings, 'ACCESS_TOKEN') and not hasattr(settings, "ACCESS_TOKEN_SECRET"):
+if not (hasattr(settings, 'ACCESS_TOKEN') and hasattr(settings, "ACCESS_TOKEN_SECRET")):
 	print "Settings are incomplete."
 else:
 	print "Everything seems to be fine, boss."
